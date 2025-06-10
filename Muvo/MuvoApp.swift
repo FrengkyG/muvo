54e20131dba3de17d6b4ed9ffdfff761c3679410
Frengky Gunawan <frengky.gg@gmail.com>
2025-06-10 08:47:20 +0700

    Initial commit

---
+++ Muvo/MuvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuvoApp.swift
+//  Muvo
+//
+//  Created by Frengky Gunawan on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MuvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

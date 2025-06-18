563e5cb2f6208546a4fb722d72e124f40854cadf
Frengky Gunawan <frengky.gg@gmail.com>
2025-06-18 09:15:29 +0700

    chore(practiceview): refactor and sync with ui

---
+++ Muvo/Views/Sentence Scorer/OnboardingPracticeView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPractice.swift
+//  Muvo
+//
+//  Created by Frengky Gunawan on 18/06/25.
+//
+
+import SwiftUI
+
+struct OnboardingPractice: View {
+    
+    var body: some View {
+        VStack{
+            Text("Halo")
+        }
+    }
+}
+

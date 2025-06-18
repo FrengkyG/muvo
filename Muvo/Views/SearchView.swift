2037d0e53931788aebf001ef380fed418d216ebb
Frengky Gunawan <frengky.gg@gmail.com>
2025-06-18 14:16:37 +0700

    feat(search): add file

---
+++ Muvo/Views/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  Muvo
+//
+//  Created by Frengky Gunawan on 18/06/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            Text("Search")
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}

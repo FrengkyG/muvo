ece9753feb16fe9be2586aac430f67c597f0a3c4
Frengky Gunawan <frengky.gg@gmail.com>
2025-06-18 09:15:29 +0700

    chore(practiceview): refactor and sync with ui

--- Muvo/Utils/ColorUtils.swift
+++ Muvo/Utils/ColorUtils.swift
@@ -12,9 +12,11 @@ extension Color {
     static let bgButtonColor = Color(hex: "#F9F6F4")
     static let greenProgressColor = Color(hex: "#00D34B")
     static let grayProgressColor = Color(hex: "#D8D8D8")
-    static let grayText = Color(hex: "B2B2B2")
+    static let grayText = Color(hex: "#B2B2B2")
     static let grayIcon = Color(hex: "#393938")
-    static let white80 = Color(hex: "EAF3FF")
+    static let white80 = Color(hex: "#EAF3FF")
+    static let greenProgressDarker = Color(hex: "#00AA13")
+    static let grayProgressLighter = Color(hex: "#F3F8FF")
 }
 
 extension Color {
